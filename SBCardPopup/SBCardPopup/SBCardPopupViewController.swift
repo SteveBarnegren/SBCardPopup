f434f4c119cecf9e1fec4817ed1562edb3fb04ca
Steve Barnegren <steve.barnegren@gmail.com>
2017-07-07 18:05:32 +0100

    Swipe to dismiss working

--- SBCardPopup/SBCardPopup/SBCardPopupViewController.swift
+++ SBCardPopup/SBCardPopup/SBCardPopupViewController.swift
@@ -39,7 +39,26 @@ public class SBCardPopupViewController: UIViewController {
         viewController.present(self, animated: false, completion: nil)
     }
     
+    // MARK: - Types
+    
+    enum State {
+        case animatingIn
+        case idle
+        case panning
+        case physicsOut(PhysicsState)
+    }
+    
+    struct PhysicsState {
+        let acceleration = CGFloat(9999)
+        var velocity = CGFloat(0)
+    }
+    
     // MARK: - Properties
+    
+    private let backgroundOpacity = CGFloat(0.6)
+    
+    private var displayLink: CADisplayLink!
+    private var lastTimeStamp: CFTimeInterval?
     
     private let contentViewController: UIViewController?
     private let contentView: UIView
@@ -49,6 +68,8 @@ public class SBCardPopupViewController: UIViewController {
     private var hasAnimatedIn = false
     
     private var containerOffscreenConstraint: NSLayoutConstraint!
+    
+    private var swipeOffset = CGFloat(0)
     
     private var popupProtocolResponder: SBCardPopupContent? {
         
@@ -62,6 +83,18 @@ public class SBCardPopupViewController: UIViewController {
         }
         
         return nil
+    }
+    
+    private var state = State.animatingIn {
+        didSet{
+            
+            switch state {
+            case .idle: print("STATE: Idle")
+            case .animatingIn: print("STATE: Animating in")
+            case .panning: print("STATE: Panning")
+            case .physicsOut: print("STATE: Physics Out")
+            }
+        }
     }
     
     // MARK: - UIViewController
@@ -99,6 +132,13 @@ public class SBCardPopupViewController: UIViewController {
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapAway))
         view.addGestureRecognizer(tapRecognizer)
         
+        // Pan Recognizer
+        let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPan))
+        view.addGestureRecognizer(panRecognizer)
+        
+        // Display Link
+        displayLink = CADisplayLink(target: self, selector: #selector(tick))
+        displayLink.add(to: .current, forMode: .commonModes)
     }
     
     public override func viewDidAppear(_ animated: Bool) {
@@ -107,6 +147,25 @@ public class SBCardPopupViewController: UIViewController {
         if !hasAnimatedIn {
             animateIn()
             hasAnimatedIn = true
+        }
+    }
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Container View
+        containerView.frame.origin.y += swipeOffset
+      
+        // Update background color if panning or physics out
+        switch state {
+        case .animatingIn: break
+        case .idle: break
+        case .panning: fallthrough
+        case .physicsOut:
+            
+            let outPct = 1.0 - (swipeOffset / view.bounds.size.height/2)
+            let opacity = backgroundOpacity * outPct
+            view.backgroundColor = UIColor(white: 0, alpha: opacity)
         }
     }
     
@@ -198,7 +257,7 @@ public class SBCardPopupViewController: UIViewController {
                        delay: 0.0,
                        options: [.curveEaseInOut],
                        animations: {
-                        self.view.backgroundColor = UIColor(white: 0, alpha: 0.6)
+                        self.view.backgroundColor = UIColor(white: 0, alpha: self.backgroundOpacity)
         }, completion: nil)
         
         // Animate container on screen
@@ -215,6 +274,7 @@ public class SBCardPopupViewController: UIViewController {
         }, completion: {
             _ in
             self.containerView.isUserInteractionEnabled = true
+            self.state = .idle
         })
     }
     
@@ -264,4 +324,107 @@ public class SBCardPopupViewController: UIViewController {
         
     }
     
+    @objc private func didPan(recognizer: UIPanGestureRecognizer) {
+        print("did pan")
+        
+        guard
+            state == .idle || state == .panning
+            else { return }
+        
+        let location = recognizer.location(in: view)
+        print("Location: \(location)")
+        
+        let applyOffset = {
+            self.swipeOffset = recognizer.translation(in: self.view).y
+            self.view.setNeedsLayout()
+        }
+        
+        switch recognizer.state {
+        case .possible:
+            break
+        case .began:
+            state = .panning
+            applyOffset()
+        case .changed:
+            applyOffset()
+        case .cancelled:
+            break
+        case .failed:
+            break
+        case .ended:
+            animateOut(fromPan: recognizer)
+        }
+    
+    }
+    
+    private func animateOut(fromPan panRecognizer: UIPanGestureRecognizer) {
+        
+//        let animateOutThreshold = CGFloat(50)
+//        
+//        let velocity = panRecognizer.velocity(in: view).y
+//        let magnitude = fabs(velocity)
+//        
+//        if magnitude > animateOutThreshold {
+//            
+//        }
+        
+        let velocity = panRecognizer.velocity(in: view).y
+        
+        let physicsState = PhysicsState(velocity: velocity)
+        state = .physicsOut(physicsState)
+    }
+    
+    // MARK: - Display Link
+    
+    func tick() {
+        
+        // We need a previous time stamp to work with, bail if we don't have one
+        guard let last = lastTimeStamp else{
+            lastTimeStamp = displayLink.timestamp
+            return
+        }
+        
+        // Work out dt
+        let dt = displayLink.timestamp - last
+        
+        // Save the current time
+        lastTimeStamp = displayLink.timestamp
+
+        
+        // If we're using physics to animate out, update
+        guard case var State.physicsOut(physicsState) = state else {
+            return
+        }
+
+        print("tick")
+        physicsState.velocity += CGFloat(dt) * physicsState.acceleration
+        
+        swipeOffset += physicsState.velocity * CGFloat(dt)
+        
+        view.setNeedsLayout()
+        state = .physicsOut(physicsState)
+        
+        // Remove if the content view is off screen
+        if swipeOffset > view.bounds.size.height / 2 {
+            dismiss(animated: false, completion: nil)
+        }
+        
+    }
+    
+}
+
+func ==(lhs: SBCardPopupViewController.State, rhs: SBCardPopupViewController.State) -> Bool {
+    
+    switch (lhs, rhs) {
+    case (.animatingIn, .animatingIn):
+        return true
+    case (.idle, .idle):
+        return true
+    case (.panning, .panning):
+        return true
+    case (.physicsOut, .physicsOut):
+        return true
+    default:
+        return false
+    }
 }
